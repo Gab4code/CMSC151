ea3e7bc79b5f01f6264cfdf6c755af9b1fd859cd
Gab <gfnatividad@up.edu.ph>
2024-12-08 00:17:43 +0800

    a bit of fix to login and register

--- helloworld/lib/screens/registerPage.dart
+++ helloworld/lib/screens/registerPage.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:helloworld/screens/registerPage.dart';
+import 'package:helloworld/screens/loginPage.dart';
 
-class LoginPage extends StatefulWidget {
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
+
   // Variable to control visibility of password
   bool _isPasswordObscured = true;
+  bool _isConfirmPasswordObscured = true;
 
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:  SingleChildScrollView( // Makes the entire page scrollable
@@ -22,17 +28,10 @@ class _LoginPageState extends State<LoginPage> {
           padding: const EdgeInsets.all(16.0), // Padding around the screen
           child:Column(
         children: [
-          Center(
-            child: Image.asset(
-              'images/Twerk.PNG', // Display the image at the top of the login page
-              width: 200, // Adjust size as needed
-              height: 200,
-              fit: BoxFit.contain,
-            ),
-          ),
+          
           SizedBox(height: 10), // Adds space between the image and the header
           Text(
-            'Login', // Header text
+            'Register', // Header text
             style: GoogleFonts.poppins(
               fontSize: 32, // Adjust size as needed
               fontWeight: FontWeight.bold,
@@ -41,7 +40,15 @@ class _LoginPageState extends State<LoginPage> {
           ),
           SizedBox(height: 10), // Adds space between the header and the subheader
           Text(
-            'Sign-in to Continue', // Subheader text
+            'Create Your Account', // Subheader text
+            style: GoogleFonts.poppins(
+              fontSize: 16, // Adjust size as needed
+              color: Colors.grey, // Color of the subheader text
+            ),
+          ),
+          SizedBox(height: 10), // Adds space between the header and the subheader
+          Text(
+            'Please Fill in Your Details', // Subheader text
             style: GoogleFonts.poppins(
               fontSize: 16, // Adjust size as needed
               color: Colors.grey, // Color of the subheader text
@@ -84,7 +91,7 @@ class _LoginPageState extends State<LoginPage> {
                   controller: emailController,
                   keyboardType: TextInputType.emailAddress, // Specifies input type as email
                   decoration: InputDecoration(
-                    hintText: 'Enter your Email Here',
+                    hintText: 'Please enter your Email Here',
                     hintStyle: TextStyle(color: Colors.grey),
                     border: OutlineInputBorder(borderSide: BorderSide.none),
                     contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
@@ -94,6 +101,51 @@ class _LoginPageState extends State<LoginPage> {
             ),
             SizedBox(height: 20), // Space between email and password fields
 
+            // Username TextField
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Username', 
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF004AAD)
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8), // Adds space between label and text field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0), // Adds padding on the sides
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white, // Background color for the container
+                  borderRadius: BorderRadius.circular(8), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1), // Shadow color with opacity
+                      spreadRadius: 2, // Spread radius for the shadow
+                      blurRadius: 5, // Blur radius for the shadow
+                      offset: Offset(0, 4), // Shadow offset, position relative to the text field
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    hintText: 'Please enter your Username Here',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: OutlineInputBorder(borderSide: BorderSide.none),
+                    contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
             // Password TextField
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -128,10 +180,11 @@ class _LoginPageState extends State<LoginPage> {
                   ],
                 ),
                  child: TextField(
-                    controller: passwordController,
                     obscureText: _isPasswordObscured, // Controls password visibility
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
                     decoration: InputDecoration(
-                      hintText: 'Enter your Password Here',
+                      hintText: 'Please enter your Password Here',
                       hintStyle: TextStyle(color: Colors.grey),
                       border: OutlineInputBorder(borderSide: BorderSide.none), // Removes default border
                       contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16), // Padding inside the text field
@@ -150,26 +203,83 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
+            SizedBox(height: 20),
+              // Password TextField
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Confirm Password', 
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF004AAD),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8), // Adds space between label and text field
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0), // Adds padding on the sides
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white, // Background color for the container
+                  borderRadius: BorderRadius.circular(8), // Rounded corners
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1), // Shadow color with opacity
+                      spreadRadius: 2, // Spread radius for the shadow
+                      blurRadius: 5, // Blur radius for the shadow
+                      offset: Offset(0, 4), // Shadow offset, position relative to the text field
+                    ),
+                  ],
+                ),
+                 child: TextField(
+                    controller: confirmPasswordController,
+                    obscureText: _isConfirmPasswordObscured, // Controls password visibility
+                    decoration: InputDecoration(
+                      hintText: 'Please Re-enter your Password',
+                      hintStyle: TextStyle(color: Colors.grey),
+                      border: OutlineInputBorder(borderSide: BorderSide.none), // Removes default border
+                      contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 16), // Padding inside the text field
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isConfirmPasswordObscured ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.grey,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isConfirmPasswordObscured = !_isConfirmPasswordObscured; // Toggle visibility
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             SizedBox(height: 30), // Adds space before the login button
 
             //LOGIN BUTTON
             ElevatedButton(
               onPressed: () {
-                // Handle login action here
-                String email = emailController.text;
-                String password = passwordController.text;
-                print('Entered email: $email');
-                print('Entered password: $password');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
               },
               child: Text(
                 'Login',
                 style: GoogleFonts.poppins(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
-                  color: Color(0xFF004AAD)
+                  color: Colors.white
                 ),
               ),
               style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF004AAD),
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
                 textStyle: TextStyle(fontSize: 18),
                 minimumSize: Size(150, 50),
@@ -180,21 +290,25 @@ class _LoginPageState extends State<LoginPage> {
             //Register BUTTON
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterPage()),
-                );
+                //Handle Register Button
+                String email = emailController.text;
+                String username = usernameController.text;
+                String password = passwordController.text;
+                String confirmPassword = confirmPasswordController.text;
+                print('Entered email: $email'); // Use the email variable
+                print('Entered username: $username');
+                print('Entered password: $password');
+                print('Entered confirmPassword: $confirmPassword');
               },
               child: Text(
                 'Register',
                 style: GoogleFonts.poppins(
                   fontSize: 18,
                   fontWeight: FontWeight.w600,
-                  color: Colors.white
+                  color: Color(0xFF004AAD)
                 ),
               ),
               style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF004AAD),
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
                 textStyle: TextStyle(fontSize: 18),
                 minimumSize: Size(150, 50),
